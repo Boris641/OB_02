8ce3f0004c0406c1796637c0ea6863f26029eaab
Boris641 <163911643+Boris641@users.noreply.github.com>
2024-04-01 08:07:40 +0400

    Update main 30.03.24.py

--- main 30.03.24.py
+++ main 30.03.24.py
@@ -1,20 +1,4 @@
-class User1():
-    def __init__(self, ND, name):
-        self._protected = ND
-        self._protected = name
-        self.users = {}
-    def nomer(self):
-        print(f"{self._protected.ND} 12345")
-    def doc(self):
-        print(f"{self._protected.name} Иван")
-    def get_protected(self):
-        return self._protected
-    def set_protected(self, name):
-        self._protected = name
 
-
-class Admin():
-    pass
 
 
 
